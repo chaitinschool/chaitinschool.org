16b1747462085f7b64c2e17c03e85cb29420d7c5
Theodore Keloglou <zf@sirodoht.com>
2021-10-11 01:32:54 +0100

    add subscription model

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from main import models
+
+
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "email",
+        "created_at",
+        "unsubscribe_key",
+    )
+
+    ordering = ["-id"]
+
+
+admin.site.register(models.Subscription, SubscriptionAdmin)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,16 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class Subscription(models.Model):
+    email = models.EmailField(unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    unsubscribe_key = models.UUIDField(default=uuid.uuid4, unique=True)
+
+    # def get_unsubscribe_url(self):
+    #    path = reverse("unsubscribe_key", args={self.unsubscribe_key})
+    #    return f"//{settings.CANONICAL_HOST}{path}"
+
+    def __str__(self):
+        return self.email
