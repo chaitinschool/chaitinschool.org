20a23df7896da7b8d50373fb3d86dff556c85101
Theodore Keloglou <zf@sirodoht.com>
2021-11-11 20:12:37 +0000

    fix settings formatting

--- chaitin/settings.py
+++ chaitin/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
